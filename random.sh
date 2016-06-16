1fb7988c54d7f6263ecb86452323899eef741ebf
dev <emangano@gmail.com>
2016-06-16 10:17:59 +0200

    added error message and starting from 1

--- random.sh
+++ random.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo $RANDOM % $1 | bc
+if [ "$#" -lt 1 ]
+then
+	echo "usage:   $(basename $0) seed"
+	echo "example: $(basename $0) 5 'to get a random number in [1,5]'"
+	exit 0
+fi
+
+echo $RANDOM % $1 + 1 | bc
