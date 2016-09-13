b4f54d661069da86c24443040d4826f8e274fb65
enri23 <emangano@gmail.com>
2016-09-13 12:56:19 +0200

    added MonitorableQueue delegating to SimpleQueue

---
+++ inheritance/src/ch/gmtech/learning/inheritance/MonitorableQueue.java
@@ -0,0 +1,28 @@
+package ch.gmtech.learning.inheritance;
+
+
+public class MonitorableQueue implements Queue {
+
+	private final Queue _queue;
+	private int _marker;
+	
+	public MonitorableQueue() {
+		_queue = new SimpleQueue();
+		_marker = 0;
+	}
+	
+	public void enqueue(Object o) {
+		_queue.enqueue(o);
+		_marker++;
+	}
+	
+	public Object dequeue() {
+		_marker--;
+		return _queue.dequeue();
+	}
+	
+	public int maximumSize() {
+		return _marker;
+	}
+	
+}
--- inheritance/src/ch/gmtech/learning/inheritance/Queue.java
+++ inheritance/src/ch/gmtech/learning/inheritance/Queue.java
@@ -1,22 +1,9 @@
 package ch.gmtech.learning.inheritance;
 
-import java.util.ArrayList;
-import java.util.List;
+public interface Queue {
 
-public class Queue {
+	void enqueue(Object o);
 
-	private List<Object> _elements;
-	
-	public Queue() {
-		_elements = new ArrayList<Object>();
-	}
-	
-	public void enqueue(Object o) {
-		_elements.add(o);
-	}
-	
-	public Object dequeue() {
-		return _elements.get(0);
-	}
-	
+	Object dequeue();
+
 }
